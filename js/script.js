d3445b839997fd373f0c9fd95cca7e2bf2ff3cd1
Marcin SNARSKI <company.m2s@gmail.com>
2019-05-20 16:36:42 +0200

    step4

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else if (randomNumber == '2') {
+    computerMove = 'papier';
+} else if (randomNumber == '3') {
+    computerMove = 'nozyce';
+} else {
+    computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
 var playerMove, playerInput;
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
     playerMove = 'kamień';
-} else {
-    playerMove = 'nieznany ruch';
-}
-if (playerInput == '2') {
+} else if (playerInput == '2') {
     playerMove = 'papier';
-} else {
-    playerMove = 'nieznany ruch';
-}
-if (playerInput == '3') {
-    playerMove = 'nozyce';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
 } else {
     playerMove = 'nieznany ruch';
 }
