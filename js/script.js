b4e7482b24060023c7bbe24a0659b1cfef0af466
Marcin SNARSKI <company.m2s@gmail.com>
2019-05-20 12:17:13 +0200

    step3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else {
+    playerMove = 'nieznany ruch';
+}
+if (playerInput == '2') {
+    playerMove = 'papier';
+} else {
+    playerMove = 'nieznany ruch';
+}
+if (playerInput == '3') {
+    playerMove = 'nozyce';
+} else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
