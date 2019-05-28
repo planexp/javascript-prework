6ea55617fe91d876397506a3cb550baa4d1485e0
Marcin SNARSKI <company.m2s@gmail.com>
2019-05-28 10:28:36 +0200

    delate description

--- js/script.js
+++ js/script.js
@@ -2,17 +2,10 @@ const buttonRock = document.getElementById('button-rock');
 const buttonPaper = document.getElementById('button-paper');
 const buttonScissors = document.getElementById('button-scissors');
 
-
-/**
- * Describe this function...
- */
 function buttonClicked(argButtonName) {
     clearMessages();
     console.log(argButtonName + ' został kliknięty');
 
-    /**
-     * Describe this function...
-     */
     function getMoveName(argMoveId) {
         console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
         if (argMoveId == 1) {
@@ -27,25 +20,21 @@ function buttonClicked(argButtonName) {
         }
     }
 
-    /**
-     * Describe this function...
-     */
     function displayResult(argPlayerMove, argComputerMove) {
         console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-        if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+        if (argPlayerMove === 'papier' && argComputerMove === 'kamień') {
             printMessage('Wygrywasz!');
-        } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+        } else if (argPlayerMove === 'kamień' && argComputerMove === 'nożyce') {
             printMessage('Wygrywasz!');
-        } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+        } else if (argPlayerMove === 'nożyce' && argComputerMove === 'papier') {
             printMessage('Wygrywasz!');
-        } else if (argPlayerMove == argComputerMove) {
+        } else if (argPlayerMove === argComputerMove) {
             printMessage('Remis!');
         } else {
             printMessage('Przegrywasz :(');
         }
         printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
     }
-
 
     const playerMove = argButtonName;
     console.log('ruch gracza to: ' + playerMove);
@@ -54,9 +43,6 @@ function buttonClicked(argButtonName) {
     const computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(playerMove, computerMove);
-
-
-
 }
 
 buttonRock.addEventListener('click', function () {
